1d1464cc03b2d42925c4f77ecc658cad3112db1f
Jakub Mastalski <25287@student.ignatianum.edu.pl>
2024-05-17 13:38:41 +0200

    Add project files.

---
+++ Chess_engine/Chess_Game_Engine/Chess_Game_Engine/BoardForm.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace ChessGameEngine {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for BoardForm
+	/// </summary>
+	public ref class BoardForm : public System::Windows::Forms::Form
+	{
+	public:
+		BoardForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~BoardForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// BoardForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(949, 532);
+			this->Name = L"BoardForm";
+			this->Text = L"BoardForm";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
